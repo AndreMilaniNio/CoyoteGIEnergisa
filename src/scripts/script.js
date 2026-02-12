23f743ff5211297f5c7a3a4f1235dd3be1a924a9
Letícia <fonleticia09@gmail.com>
2026-02-12 10:30:39 -0300

    subindo projeto

---
+++ src/scripts/script.js
@@ -0,0 +1,18 @@
+const elements = document.querySelectorAll(".fade-in");
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("visible");
+      } else {
+        entry.target.classList.remove("visible");
+      }
+    });
+  },
+  {
+    threshold: 0.2, // ativa quando 20% do elemento estiver visível
+  }
+);
+
+elements.forEach((el) => observer.observe(el));
